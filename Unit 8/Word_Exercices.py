9dd34f0bd5c583d35c78303bb6e8e855b4629416
Drael Davidson <drael_davidson@hotmail.com>
2023-03-17 00:17:19 -0700

    Update Word_Exercices.py

--- Unit 8/Word_Exercices.py
+++ Unit 8/Word_Exercices.py
@@ -2,27 +2,109 @@
 
 fin = open('Unit 8/words.txt')
 
-def print_all(fin):
-    for line in fin:
-        word = line.strip()
-        print(word)
-
-# Write a program that reads words.txt and prints only the words with more than 20 characters (not counting whitespace)
-def print_over_20(txt):
+def print_fin(txt):
+    counter = 0
     for line in txt:
-        if len(line) > 20:
-            word = line.strip()
-            print(word)
+        word = line.strip()
+        counter += 1
+        print(word)
+    print(counter)
 
-print_over_20(fin)
+# print_fin(fin)
+
+# ⭐️ Write a program that reads words.txt and prints only the words with more than 20 characters (not counting whitespace)
+def print_over_20(word):
+    if len(word) > 20:
+        print(word)
+    
+
+#print_over_20(fin)
 
 
-# Write a function called has_no_e that returns True if the given word doesn’t have the letter “e” in it.
+# ⭐️ Write a function called has_no_e that returns True if the given word doesn’t have the letter “e” in it.
 def has_no_e(word):
     e = word.find('e')
     if e == -1:
+        print(word)
         return True
 
+#has_no_e('word')
+
+# ⭐️ Write a program that reads words.txt and prints only the words that have no “e”. 
+def no_e(txt):
+    counter = 0
+    total = 0
+    for line in txt:
+        total += 1
+        e = line.find('e')
+        if e == -1:
+            word = line.strip()
+            counter += 1
+            print(word)
+    percentage = int(counter / total * 100)
+    # Compute the percentage of words in the list that have no “e”.
+    print(f'{percentage}% of words in the list dont have the letter "e".')
+
+# print('printing no e')
+# no_e(fin)
+
+# ⭐️ Write a function named avoids that takes a word and a string of forbidden letters
+
+def avoids(word,string):
+    for letter in string:
+        if letter in word:
+            return False
+    print(word)
+    return True   
+
+# avoids('happy','bvf')
 
 
-has_no_e('word')
+
+
+
+# Write a function named uses_only that takes a word and a string of letters, and that returns True if the word contains only letters in the list.
+
+def uses_only(word,uses):
+    for letter in word:
+        if letter not in uses:
+            return False
+    print(word)
+    return True
+
+
+# Write a function named uses_all that takes a word and a string of required letters, and that returns True if the word uses all the required letters at least once.
+
+def uses_all(word,all):
+    for letter in all:
+        if letter not in word:
+            return False
+    print(word)
+    return True
+
+
+# Write a function called is_abecedarian that returns True if the letters in a word appear in alphabetical order (double letters are ok)
+
+def is_abecedarian(word):
+    sort = ''.join(sorted(word))
+    if word == sort:
+        print(word)
+
+# is_abecedarian('aabcd')
+
+
+def print_all(txt): 
+    for line in txt:
+        word = line.strip()
+        print_over_20(word)
+        # has_no_e(word)
+        # avoids(word,'eario')
+        # uses_only(word,'acefhlo')
+        # uses_all(word,'aeiouy')
+        # is_abecedarian(word)
+        
+
+# print_all(fin)
+
+
+
